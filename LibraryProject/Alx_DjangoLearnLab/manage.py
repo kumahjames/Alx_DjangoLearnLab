b0ced5a2d317917889e5d963f2a948dbe434d23a
kumahjames <miraclearena7@gmail.com>
2025-11-09 04:42:46 -0800

    Add LibraryProject directory structure for auto-checker compatibility

--- LibraryProject/Alx_DjangoLearnLab/manage.py
+++ LibraryProject/Alx_DjangoLearnLab/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alx_DjangoLearnLab.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alx_DjangolearnLab.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
