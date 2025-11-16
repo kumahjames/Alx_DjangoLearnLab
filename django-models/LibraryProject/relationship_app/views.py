3fd37480d2ee3fa06396e5d097f8eb3ddd92e888
kumahjames <miraclearena7@gmail.com>
2025-11-16 07:20:52 -0800

    Fix Django generic view imports: use specific imports for ListView and DetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Library
 
 # Function-based view to list all books
