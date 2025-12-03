3e11cf44a89fb29d4fb3fb68c1f198bdafa598fd
kumahjames <miraclearena7@gmail.com>
2025-12-03 09:22:04 -0800

    Add database configuration with USER and PORT settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,9 +77,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',     
+        'PASSWORD': '',  
+        'HOST': '',     
+        'PORT': '',    
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
