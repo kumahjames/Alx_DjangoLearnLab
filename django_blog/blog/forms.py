acebca82fd02953531bd7741a2362d5cc095b8fb
kumahjames <miraclearena7@gmail.com>
2025-12-03 22:00:17 -0800

    fix: Add TagWidget to PostForm as required by checker

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagWidget  # Add this import
 
 # Custom registration form that extends Django's UserCreationForm
 class UserRegistrationForm(UserCreationForm):
@@ -28,24 +29,18 @@ class UserUpdateForm(forms.ModelForm):
 
 # Form for creating and updating blog posts WITH TAGS
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        help_text='Enter tags separated by commas (e.g., django, python, web)',
-        widget=forms.TextInput(attrs={'placeholder': 'django, python, web'})
-    )
-    
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {  # Add this widgets configuration
+            'tags': TagWidget(),
+        }
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['title'].widget.attrs.update({'placeholder': 'Enter post title'})
         self.fields['content'].widget.attrs.update({'placeholder': 'Write your post content here...'})
-        
-        # Set initial tags value if editing existing post
-        if self.instance and self.instance.pk:
-            self.initial['tags'] = ', '.join(tag.name for tag in self.instance.tags.all())
+        self.fields['tags'].help_text = 'Enter tags separated by commas (e.g., django, python, web)'
 
 # Form for creating and updating comments
 class CommentForm(forms.ModelForm):
