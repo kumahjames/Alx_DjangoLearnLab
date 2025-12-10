8fa323174bac33e9dad6232d6166c5263d7209a7
kumahjames <miraclearena7@gmail.com>
2025-12-10 11:11:55 -0800

    fix: Update serializers with checker requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,24 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password', 'bio']
         extra_kwargs = {
             'email': {'required': True},
         }
     
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            password=validated_data['password']
         )
+        user.bio = validated_data.get('bio', '')
+        user.save()
+        Token.objects.create(user=user)
         return user
 
 class UserLoginSerializer(serializers.Serializer):
@@ -48,7 +53,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     following_count = serializers.IntegerField(read_only=True)
     
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 
                   'followers_count', 'following_count', 'date_joined']
         read_only_fields = ['id', 'date_joined']
