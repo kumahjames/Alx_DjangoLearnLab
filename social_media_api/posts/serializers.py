cddab200a243402ae7bb3133a2645ecc48b50924
kumahjames <miraclearena7@gmail.com>
2025-12-10 20:19:51 -0800

    feat: Add posts and comments functionality

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Post, Comment
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'author']
+    
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    comments_count = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'created_at', 
+                  'updated_at', 'comments', 'comments_count']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'author', 
+                           'comments', 'comments_count']
+    
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+class PostDetailSerializer(PostSerializer):
+    # More detailed view of post
+    class Meta(PostSerializer.Meta):
+        fields = PostSerializer.Meta.fields
---
+++ social_media_api/posts/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets, filters, status
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Comment
+from .serializers import (
+    PostSerializer, 
+    PostDetailSerializer,
+    CommentSerializer
+)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing posts.
+    """
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author']
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
+    
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return PostDetailSerializer
+        return PostSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Filter by title/content if search parameter provided
+        search = self.request.query_params.get('search', None)
+        if search:
+            queryset = queryset.filter(
+                title__icontains=search
+            ) | queryset.filter(
+                content__icontains=search
+            )
+        return queryset.distinct()
+    
+    @action(detail=True, methods=['get'], permission_classes=[IsAuthenticatedOrReadOnly])
+    def comments(self, request, pk=None):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing comments.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        post_id = self.request.query_params.get('post', None)
+        if post_id:
+            queryset = queryset.filter(post_id=post_id)
+        return queryset
